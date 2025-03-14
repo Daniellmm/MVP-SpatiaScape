6f3f0a27cf861daa7c971e28b55f1a41f06d67fd
Daniellmm <idowudanielcsc190400@gmail.com>
2025-03-14 07:50:01 -0700

    url updated

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5500/api/v1',
+  // baseURL: 'http://localhost:5500/api/v1',
+  baseURL: 'https://spatiascape-mpv-backend.onrender.com/api/v1',
   withCredentials: true, 
 });
 
